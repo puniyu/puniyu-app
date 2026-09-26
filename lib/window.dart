36580960eb32c89f3f54ce1cc774f78b4188c4be
wuliya336 <3369906077@qq.com>
2026-09-26 16:05:40 +0800

    refactor(window): 移除静态常量定义并内联最小窗口尺寸

--- lib/window.dart
+++ lib/window.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/widgets.dart';
 import 'package:window_manager/window_manager.dart';
 
-class WindowManger {
-  static const Size _size = Size(1024, 768);
-  static const _minimumSize = Size(800, 600);
+const Size _size = Size(1024, 768);
 
+class WindowManger {
   static Future<void> initialize() async {
     await windowManager.ensureInitialized();
     WindowOptions windowOptions = WindowOptions(
       size: _size,
-      minimumSize: _minimumSize,
+      minimumSize: Size(800, 600),
       center: true,
       backgroundColor: const Color(0x00000000),
       skipTaskbar: false,
